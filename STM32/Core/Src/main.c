780c02ae221078cff2c4c5d1a377551d18b6adde
Kamil Walczak <waperr@interia.pl>
2026-01-02 20:55:04 +0100

    fix sed command, remove comment

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -308,7 +308,6 @@ void SendTelemetry(void) {
     // 1. Nagłówek LoRa: < (0x3C), 0xFF, 0x01
     // 2. Nagłówek AX.25: Źródło>Cel,Ścieżka:
     // 3. Dane APRS: !Lat/Lon#Komentarz
-    //sprintf(packet, "\x3c\xff\x01SP7FM-1>APRS,WIDE1-1:!5144.22N/01934.44E#SP7FM-1 BAT:%.2fV", voltage);
     sprintf(packet, "\x3c\xff\x01%s>APRS,WIDE1-1:%s#%s BAT:%.2fV", APRS_CALLSIGN, APRS_COORDS, APRS_CALLSIGN, voltage);
 
     DebugPrint("TELEMETRY: %s\r\n", packet + 3); // +3 żeby nie wyświetlać krzaków w logu
